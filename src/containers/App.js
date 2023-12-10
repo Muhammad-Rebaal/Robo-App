f8fc824d94bcf4ff125765509dfa2683556d48b9
Muhammad-Rebaal <mrebaal14@gmail.com>
2023-12-10 12:41:41 +0500

    Added

---
+++ src/containers/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setRobots(users));
+  }, []);
+
+  const onSearchChange = event => {
+    setSearchfield(event.target.value);
+  }
+
+  const filteredRobots = robots.filter(robot =>
+    robot.name.toLowerCase().includes(searchfield.toLowerCase())
+  );
+
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className='tc'>
+      <h1 className='f1'>RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+import { CHANGE_SEARCH_FIELD } from "./constants";
+
+const initialState ={
+    searchField:''
+}
+export const searchRobots = (state=initialState,action={}) =>{
+    switch(action.type){
+        case:CHANGE_SEARCH_FIELD:
+        return Object.assign({},state,searchField:action.payload)
+
+    }
+}
